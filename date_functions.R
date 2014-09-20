67a31d69c04a4ba33c8631c7ed4f9e34223a6fa2
davidcorton <david.orton@gmail.com>
2014-09-20 18:45:50 +0100

    add anatomical comparison function'anat.simulate'; moved variants on freq.simulate into separate file, minor tweaks to existing code

--- date_functions.R
+++ date_functions.R
@@ -204,112 +204,103 @@ freq.simulate <- function(data, probs, filter.field="Species", filter.values=NUL
     list(results, summary)
 }
 
-
-# Variation of freq.simulate. Renamed freq.simulate.species so not to be confused with the elements freq.simulate. Boxplot graphs included for read and dummy data
-
-freq.simulate.species <- function(data, probs, filter.field="Species", filter.values=NULL, quant.list=c(0.025,0.25,0.5,0.75,0.975), start.date=0, end.date=2000, rep=1000) {
-  require(data.table)
-  require(reshape2)
-  data <- data.table(data)  #just in case it isn't already in this format
-  if(is.null(filter.values)==FALSE) {  #if criteria have been provided...
-    setnames(data, old=filter.field, new="FILTER")   #...sets the filter column...
-    data <- data[FILTER %in% filter.values,]  #...and applies the criteria
-  } else {filter.values <- "ALL"}
-  data <- data[End >= start.date & Start <= end.date]  #drops records outside the date range FROM BOTH SIMULATION SETS
-  bin.width <- (end.date-start.date)/nrow(probs)  #sets bin widths (and hence no. of bins) to match the calibration dataset
+anat.simulate <- function(data, probs, filter.field="group", filter.values=c("cranial", "postcranial"), quant.list=c(0.025,0.25,0.5,0.75,0.975), start.date=0, end.date=2000, rep=1000) {
+    require(data.table)
+    require(reshape2)
+    data <- data.table(data) 
+    setnames(data, old=filter.field, new="FILTER")
+    data <- data[End >= start.date & Start <= end.date]
+    bin.width <- (end.date-start.date)/length(probs)
+    comparison <<- filter.values
+    
+    # set up list of all bins and rep no.s
+    breaks <- seq(start.date, end.date, bin.width)
+    labels <- numeric(length(probs))
+    for(i in 1:length(labels)) {
+        labels[i] <- paste(breaks[i], breaks[i+1], sep="-")
+    } 
+    frame <- data.table(rep(1:rep, each=length(labels)), rep(1:length(labels), rep), as.character(rep(labels, rep)))
+    setnames(frame, old=c("V1", "V2", "V3"), new=c("rep.no", "bin.no", "bin"))
+    
+    # run simulation on each group
+    sim.1 <- date.simulate(data[FILTER==filter.values[1],list(Start, End)], start.date=start.date, end.date=end.date, bin.width=bin.width, rep=rep, weight=data[FILTER==filter.values[1],Frag])
+    sim.2 <- date.simulate(data[FILTER==filter.values[2],list(Start, End)], start.date=start.date, end.date=end.date, bin.width=bin.width, rep=rep, weight=data[FILTER==filter.values[2],Frag]) 
+    real <- merge(sim.1, sim.2, by=c("rep.no", "bin"), all=TRUE)
+    rm(sim.1, sim.2)
+    setnames(real, old=c("V1.x", "V1.y"), new=c("real.1", "real.2"))
+    
+    # run dummy simulation on each group
+    dummy.1 <- dummy.simulate(probs, data[FILTER==filter.values[1], Frag], start.date=start.date, end.date=end.date, rep=rep)
+    dummy.2 <- dummy.simulate(probs, data[FILTER==filter.values[2], Frag], start.date=start.date, end.date=end.date, rep=rep) 
+    dummy <- merge(dummy.1, dummy.2, by=c("rep.no", "bin"), all=TRUE)
+    rm(dummy.1, dummy.2)
+    setnames(dummy, old=c("V1.x", "V1.y"), new=c("dummy.1", "dummy.2"))
+        
+    # combine results
+    combined <- merge(real, dummy, by=c("rep.no", "bin"), all=TRUE)
+    combined <- merge(combined, frame, by=c("rep.no", "bin"), all=TRUE)
+    
+    # replace NAs with 0s
+    combined[is.na(real.1)==TRUE, real.1:=0]
+    combined[is.na(real.2)==TRUE, real.2:=0]
+    combined[is.na(dummy.1)==TRUE, dummy.1:=0]
+    combined[is.na(dummy.2)==TRUE, dummy.2:=0]
+    
+    # calculate differences
+    combined[,diff:=(real.1-real.2)/(real.1+real.2)]
+    combined[,dummy.diff:=dummy.1-dummy.2]
+    
+    # calculate rates of change
+    for(i in 1:(nrow(combined)-1)) {
+        combined[i,ROC.real.1:=(combined[i+1,real.1]-combined[i,real.1])/bin.width]
+        combined[i,ROC.dummy.1:=(combined[i+1,dummy.1]-combined[i,dummy.1])/bin.width]
+        combined[i,ROC.real.2:=(combined[i+1,real.2]-combined[i,real.2])/bin.width]
+        combined[i,ROC.dummy.2:=(combined[i+1,dummy.2]-combined[i,dummy.2])/bin.width]
+        combined[i,ROC.diff:=(combined[i+1,diff]-combined[i,diff])/bin.width]
+        combined[i,ROC.dummy.diff:=(combined[i+1,dummy.diff]-combined[i,dummy.diff])/bin.width]
+        if(i/500 == round(i/500)) {print(paste(i/nrow(combined)*100, "percent complete"))}
+    }
+    combined[bin==labels[length(labels)], ROC.real.1:=NA]
+    combined[bin==labels[length(labels)], ROC.dummy.1:=NA]
+    combined[bin==labels[length(labels)], ROC.real.2:=NA]    
+    combined[bin==labels[length(labels)], ROC.dummy.2:=NA]
+    combined[bin==labels[length(labels)], ROC.diff:=NA]
+    combined[bin==labels[length(labels)], ROC.dummy.diff:=NA]
+    
+    
+    # create summary dataset
+    real.1.summary <- combined[,quantile(real.1, probs=quant.list, na.rm=TRUE), by=bin]
+    real.1.summary[,id:=paste(rep("real.1", length(quant.list)), quant.list, sep="_")]
+    real.1.summary <- dcast.data.table(real.1.summary, bin ~ id, value.var="V1")
+    
+    real.2.summary <- combined[,quantile(real.2, probs=quant.list, na.rm=TRUE), by=bin]
+    real.2.summary[,id:=paste(rep("real.2", length(quant.list)), quant.list, sep="_")]
+    real.2.summary <- dcast.data.table(real.2.summary, bin ~ id, value.var="V1")
+    
+    dummy.1.summary <- combined[,quantile(dummy.1, probs=quant.list, na.rm=TRUE), by=bin]
+    dummy.1.summary[,id:=paste(rep("dummy.1", length(quant.list)), quant.list, sep="_")]
+    dummy.1.summary <- dcast.data.table(dummy.1.summary, bin ~ id, value.var="V1")
   
-  # simulate from real data
-  real <- date.simulate(data=data[,list(Start, End)], weight=data[,Frag], bin.width=bin.width, start.date=start.date, end.date=end.date, rep=rep)
-  setnames(real, old="V1", new="real")
-  boxplot(real ~ bin, data=real, main=paste(filter.values, "Frequency simulate"), xlab="Time (50yr intervals)", ylab="Frequency")
-  
-  
-  # simulate dummy set
-  dummy <- dummy.simulate(probs=probs, weight=data[,Frag], start.date=start.date, end.date=end.date, rep=rep)    
-  setnames(dummy, old="V1", new="dummy")
-  boxplot(dummy ~ bin, data=dummy, main=paste(filter.values,"Frequency dummy"), xlab="Time (50yr intervals)", ylab="Frequency")
-  
-  # set up list of all bins and rep no.s
-  labels <- numeric(nrow(probs))
-  for(i in 1:length(labels)) {
-    labels[i] <- paste(breaks[i], breaks[i+1], sep="-")
-  } 
-  frame <- data.table(rep(1:rep, each=length(labels)), rep(labels, rep))
-  setnames(frame, old=c("V1", "V2"), new=c("rep.no", "bin"))
-  
-  # merge the above three data.tables together
-  results <- merge(frame, dummy, by=c("rep.no", "bin"), all=TRUE)
-  results <- merge(results, real, by=c("rep.no", "bin"), all=TRUE)
-  results[is.na(real)==TRUE, real:=0] 
-  results[is.na(dummy)==TRUE, dummy:=0]
-  
-  # save full dataset
-  write.csv(results, paste("TEST_", filter.values[1], "_simulated_by_period", params, ".csv",sep=""), row.names=FALSE)
-  
-  # create summary dataset
-  real.summary <- results[,quantile(real, probs=quant.list), by=bin]
-  real.summary[,id:=paste(rep("real", length(quant.list)), quant.list, sep="_")]
-  real.summary <- dcast.data.table(real.summary, bin ~ id, value.var="V1")
-  dummy.summary <- results[,quantile(dummy, probs=quant.list), by=bin]
-  dummy.summary[,id:=paste(rep("dummy", length(quant.list)), quant.list, sep="_")]
-  dummy.summary <- dcast.data.table(dummy.summary, bin ~ id, value.var="V1")
-  summary <- cbind(real.summary, dummy.summary)
-  
-  #save summary dataset
-  write.csv(summary, paste("TEST_summary_", filter.values[1], "_simulated_by_period", params, ".csv", sep=""), row.names=FALSE)
-  summary
+    dummy.2.summary <- combined[,quantile(dummy.2, probs=quant.list, na.rm=TRUE), by=bin]
+    dummy.2.summary[,id:=paste(rep("dummy.2", length(quant.list)), quant.list, sep="_")]
+    dummy.2.summary <- dcast.data.table(dummy.2.summary, bin ~ id, value.var="V1")
+    
+    ROC.dummy.1.summary <- combined[, quantile(ROC.dummy.1, probs=quant.list, na.rm=TRUE), by=bin]
+    ROC.dummy.1.summary[,id:=paste(rep("ROC.dummy.1", length(quant.list)), quant.list, sep="_")]
+    ROC.dummy.1.summary <- dcast.data.table(ROC.dummy.1.summary, bin ~ id, value.var="V1")
+    
+    ROC.dummy.2.summary <- combined[, quantile(ROC.dummy.2, probs=quant.list, na.rm=TRUE), by=bin]
+    ROC.dummy.2.summary[,id:=paste(rep("ROC.dummy.2", length(quant.list)), quant.list, sep="_")]
+    ROC.dummy.2.summary <- dcast.data.table(ROC.dummy.2.summary, bin ~ id, value.var="V1")
+    
+    ROC.dummy.diff.summary <- combined[, quantile(ROC.dummy.diff, probs=quant.list, na.rm=TRUE), by=bin]
+    ROC.dummy.diff.summary[,id:=paste(rep("ROC.dummy.diff", length(quant.list)), quant.list, sep="_")]
+    ROC.dummy.diff.summary <- dcast.data.table(ROC.dummy.diff.summary, bin ~ id, value.var="V1")
+    
+    summary <- cbind(real.1.summary, real.2.summary, dummy.1.summary, dummy.2.summary, ROC.dummy.1.summary, ROC.dummy.2.summary, ROC.dummy.diff.summary)
+    for(i in 1:6){summary[,bin:=NULL]}
+    summary <- merge(combined[rep.no==1, list(bin, bin.no)], summary, by="bin")
+    
+    list(combined[order(rep.no, bin.no)], summary[order(bin.no)])
 }
 
-# A variation of freq.simulate used for filtering by Elements.. Renamed freq.simulate.element so not to be confused with the species freq.simulate. Boxplot graphs included for read and dummy data.
-# Further work needed on boxplot and CSV files to include speices name as well as the filter.values. 
-freq.simulate.element <- function(data, probs, filter.field="Elements", filter.values=NULL, quant.list=c(0.025,0.25,0.5,0.75,0.975), start.date=0, end.date=2000, rep=1000) {
-  require(data.table)
-  require(reshape2)
-  data <- data.table(data)  #just in case it isn't already in this format
-  if(is.null(filter.values)==FALSE) {  #if criteria have been provided...
-    setnames(data, old=filter.field, new="FILTER")   #...sets the filter column...
-    data <- data[FILTER %in% filter.values,]  #...and applies the criteria
-  } else {filter.values <- "ALL"}
-  data <- data[End >= start.date & Start <= end.date]  #drops records outside the date range FROM BOTH SIMULATION SETS
-  bin.width <- (end.date-start.date)/nrow(probs)  #sets bin widths (and hence no. of bins) to match the calibration dataset
-  
-  # simulate from real data
-  real <- date.simulate(data=data[,list(Start, End)], weight=data[,Frag], bin.width=bin.width, start.date=start.date, end.date=end.date, rep=rep)
-  setnames(real, old="V1", new="real")
-  boxplot(real ~ bin, data=real, main=paste(filter.values[1], "Frequency simulate"), xlab="Time (50yr intervals)", ylab="Frequency")
-  
-  # simulate dummy set
-  dummy <- dummy.simulate(probs=probs, weight=data[,Frag], start.date=start.date, end.date=end.date, rep=rep)    
-  setnames(dummy, old="V1", new="dummy")
-  boxplot(dummy ~ bin, data=dummy, main=paste(filter.values[1],"Frequency dummy"), xlab="Time (50yr intervals)", ylab="Frequency")
-  
-  # set up list of all bins and rep no.s
-  labels <- numeric(nrow(probs))
-  for(i in 1:length(labels)) {
-    labels[i] <- paste(breaks[i], breaks[i+1], sep="-")
-  } 
-  frame <- data.table(rep(1:rep, each=length(labels)), rep(labels, rep))
-  setnames(frame, old=c("V1", "V2"), new=c("rep.no", "bin"))
-  
-  # merge the above three data.tables together
-  results <- merge(frame, dummy, by=c("rep.no", "bin"), all=TRUE)
-  results <- merge(results, real, by=c("rep.no", "bin"), all=TRUE)
-  results[is.na(real)==TRUE, real:=0] 
-  results[is.na(dummy)==TRUE, dummy:=0]
-  
-  # save full dataset
-  write.csv(results, paste(filter.values[1], "_simulated_by_period", params, ".csv",sep=""), row.names=FALSE)
-  
-  # create summary dataset
-  real.summary <- results[,quantile(real, probs=quant.list), by=bin]
-  real.summary[,id:=paste(rep("real", length(quant.list)), quant.list, sep="_")]
-  real.summary <- dcast.data.table(real.summary, bin ~ id, value.var="V1")
-  dummy.summary <- results[,quantile(dummy, probs=quant.list), by=bin]
-  dummy.summary[,id:=paste(rep("dummy", length(quant.list)), quant.list, sep="_")]
-  dummy.summary <- dcast.data.table(dummy.summary, bin ~ id, value.var="V1")
-  summary <- cbind(real.summary, dummy.summary)
-  
-  #save summary dataset
-  write.csv(summary, paste("Species_element_summary_", filter.values[1], "_simulated_by_period", params, ".csv", sep=""), row.names=FALSE)
-  summary
-}
